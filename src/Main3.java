3cb2b29fcf5714722cfdf31fc9376e3dfe730078
sehyeon <gs1884@naver.com>
2024-02-19 12:41:41 +0900

    처음 만들어본 파일

---
+++ src/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("아껴주시고");
+        System.out.println("배려해주시고");
+        System.out.println("성원해주시고");
+        System.out.println("응원해주시고");
+        System.out.println("염려해주시고");
+
+    }
+}
