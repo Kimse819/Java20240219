3ac418a717c3890be202364da3384661def32999
sehyeon <gs1884@naver.com>
2024-02-20 16:32:53 +0900

    02-20 실습 전체

---
+++ src/c02/lecture/p02type/C20TypeConversion.java
@@ -0,0 +1,14 @@
+package c02.lecture.p02type;
+
+public interface C20TypeConversion {
+    public static void main(String[] args) {
+        //정수형 : byte(1), short(2), int(4), long(8)
+        //실수형 : float(4), boolean(
+        byte a = 23;
+        int b = a;
+        short c = a;
+        long d = a;
+        
+
+    }
+}
