4c703b5bcd567df08fa3d2a2709240e9746f0d7a
sehyeon <gs1884@naver.com>
2024-02-21 17:50:25 +0900

    02-21 실습 전체

--- src/C03/sec02/IncreaseDecreaseOperatorEx.java
+++ src/C03/sec02/IncreaseDecreaseOperatorEx.java
@@ -6,29 +6,36 @@ public class IncreaseDecreaseOperatorEx {
         int y = 10;
         int z;
 
+        //1번
         x++;
         ++x;
         System.out.println("x = " + x);
 
+        //2번
         System.out.println("----------------");
         y--;
         --y;
         System.out.println("y = " + y);
 
+        //3번
         System.out.println("----------------");
         z = x++;
         System.out.println("z = " + z);
         System.out.println("x = " + x);
 
+        //4번
         System.out.println("----------------");
         z = ++x;
         System.out.println("z = " + z);
         System.out.println("x = " + x);
 
+        //5번
         System.out.println("----------------");
         z = ++x + y++;
         System.out.println("z = " + z);
         System.out.println("x = " + x);
         System.out.println("y = " + y);
+
+
     }
 }
