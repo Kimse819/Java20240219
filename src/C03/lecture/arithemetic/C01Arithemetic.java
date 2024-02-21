fec0a2389e3d758a6cc957cbcea774b12cd276e4
sehyeon <gs1884@naver.com>
2024-02-21 16:39:24 +0900

    02-21 실습 전체

---
+++ src/C03/lecture/arithemetic/C01Arithemetic.java
@@ -0,0 +1,14 @@
+package C03.lecture.arithemetic;
+
+public class C01Arithemetic {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 9;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b); //b를 a로 나눈 나머지
+    }
+}
