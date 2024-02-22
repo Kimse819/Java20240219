9faf37e07f4c465fd663edcdf15714215d2f21ad
sehyeon <gs1884@naver.com>
2024-02-22 16:21:43 +0900

    연산자 실습

--- src/C03/Exercise/Checkprop107.java
+++
@@ -1,7 +0,0 @@
-package C03.Exercise;
-
-public class Checkprop107 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/C03/logical/C02Not.java
@@ -0,0 +1,13 @@
+package C03.logical;
+
+public class C02Not {
+    public static void main(String[] args) {
+
+        boolean b = !true;
+        boolean b1 = !false;
+
+        System.out.println("b = " + b);
+        System.out.println("b1 = " + b1);
+
+    }
+}
