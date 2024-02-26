647fbe841001bfe68d7db026a518c7df8359d15e
sehyeon <gs1884@naver.com>
2024-02-26 16:41:02 +0900

    Do While 실습

---
+++ src/Ch04/lecture/p3while/C04DoWhile.java
@@ -0,0 +1,16 @@
+package Ch04.lecture.p3while;
+
+public class C04DoWhile {
+
+    public static void main(String[] args) {
+        System.out.println("statement1");
+
+        boolean b = true;
+
+        do {
+            System.out.println("statement2");
+        } while (b);
+
+        System.out.println("statement3");
+    }
+}
