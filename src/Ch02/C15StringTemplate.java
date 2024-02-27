8aa9699f1144a9a36257df0414857ffe07f06b0f
sehyeon <gs1884@naver.com>
2024-02-27 12:39:14 +0900

    오류사항 수정(java21)

--- src/Ch02/C15StringTemplate.java
+++ src/Ch02/C15StringTemplate.java
@@ -3,27 +3,32 @@ package Ch02;
 import static java.lang.StringTemplate.STR;
 
 public class C15StringTemplate {
-    public static void main(String[] args) {
-        //java21
-        //String template
+   public static void main(String[] args) {
+        // java 21
+        // string template
 
         String name = "jane";
         String age = "50";
 
-        String desc = " my name is " + name + "and age is " + age; //1번
-        System.out.println(desc);
+        // my name is jane and age is 50
 
-        String desc2 = String.format("my name is %s and age is %s", name, age); //2번
+        String desc1 = "my name is " + name + " and age is " + age;
+        System.out.println(desc1);
+
+        String desc2 = String.format("my name is %s and age is %s", name, age);
         System.out.println(desc2);
 
-        String desc3 = STR."my name is \{name} and age is \{age}"; //3번
+        String desc3 = STR."my name is \{name} and age is \{age}";
         System.out.println(desc3);
 
         //
         String address = "서울";
         String country = "한국";
 
+        // 저는 한국에 있는 서울에 삽니다.
         String desc4 = STR."저는 \{country}에 있는 \{address}에 삽니다.";
         System.out.println(desc4);
+
+
     }
 }
