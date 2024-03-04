35c0ea602f4959bd7bf398f2fc558025395ba59e
sehyeon <gs1884@naver.com>
2024-03-04 16:36:31 +0900

    6장 확인문제 13번

---
+++ src/ch06/Exercise/p13/Member.java
@@ -0,0 +1,12 @@
+package ch06.Exercise.p13;
+
+public class Member {
+    //이름, 아이디, 패스워드, 나이
+    String name;
+    String id;
+
+    String password;
+    int age;
+
+
+    }
---
+++ src/ch06/Exercise/p13/MemberEx.java
@@ -0,0 +1,14 @@
+package ch06.Exercise.p13;
+
+public class MemberEx {
+    public static void main(String[] args) {
+        Member member1 = new Member();
+        member1.name = "son";
+
+        Member member2 = new Member();
+        member2.name = "Lee";
+
+
+
+    }
+}
