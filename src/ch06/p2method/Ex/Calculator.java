62f323ec051f0d145b55fcc76ce8c369d4bf66e2
sehyeon <gs1884@naver.com>
2024-03-04 16:40:49 +0900

    Parameter

---
+++ src/ch06/p2method/Ex/Calculator.java
@@ -0,0 +1,12 @@
+package ch06.p2method.Ex;
+
+public class Calculator {
+    double areaRectangle(double width) {
+        return width * width;
+    }
+    double areaRectangle(double width, double height) {
+        return width * height;
+    }
+
+}
+
