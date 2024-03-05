f049ab8c995052aa9960dc49cf102ac87c27faa6
sehyeon <gs1884@naver.com>
2024-03-05 16:42:22 +0900

    import

---
+++ src/ch06/p4Package/hyundai/Car.java
@@ -0,0 +1,10 @@
+package ch06.p4Package.hyundai;
+
+import ch06.p4Package.hankook.SnowTire;
+import ch06.p4Package.kumho.AllSeasonTire;
+public class Car {
+    ch06.p4Package.hankook.Tire tire1 = new ch06.p4Package.hankook.Tire();
+    ch06.p4Package.kumho.Tire tire2 = new ch06.p4Package.kumho.Tire();
+    SnowTire tire3 = new SnowTire();
+    AllSeasonTire tire4 = new AllSeasonTire();
+}
