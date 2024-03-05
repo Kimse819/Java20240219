080ddbecdfe07dd0c3e5c5f6b0f0ff0eb0fc1952
sehyeon <gs1884@naver.com>
2024-03-05 11:26:26 +0900

    P.223 예제

---
+++ src/ch06/p2method/Ex/Korean.java
@@ -0,0 +1,12 @@
+package ch06.p2method.Ex;
+
+public class Korean {
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    Korean(String n, String s){
+        name = n;
+        ssn = s;
+    }
+}
---
+++ src/ch06/p2method/Ex/KoreanEx.java
@@ -0,0 +1,18 @@
+package ch06.p2method.Ex;
+
+public class KoreanEx {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("김세현", "030309-1234567");
+
+        System.out.println("k1.nation :" + k1.nation);
+        System.out.println("k1.name :" + k1.name);
+        System.out.println("k1.ssn :" + k1.ssn);
+        System.out.println();
+
+        Korean k2 = new Korean("김현수", "030617-8910112");
+
+        System.out.println("k2.nation :" + k2.nation);
+        System.out.println("k2.name :" + k2.name);
+        System.out.println("k2.ssn :" + k2.ssn);
+    }
+}
