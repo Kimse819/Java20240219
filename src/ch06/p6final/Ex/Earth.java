8c2a99c0082a76f99901640d03503ae3062a332b
sehyeon <gs1884@naver.com>
2024-03-06 11:34:57 +0900

    static final

---
+++ src/ch06/p6final/Ex/Earth.java
@@ -0,0 +1,12 @@
+package ch06.p6final.Ex;
+
+public class Earth {
+
+    static final double EARTH_RADIUS = 6400;
+    static final double EARTH_SURFACE_AREA;
+
+    static {
+        EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+    }
+
+}
