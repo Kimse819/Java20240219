3891585694baafc3006fae52af763a2f4e3d4aac
sehyeon <gs1884@naver.com>
2024-03-07 16:46:43 +0900

    확인문제 8번

---
+++ src/ch07/p2Polymorphism/Exam/SnowTireEx.java
@@ -0,0 +1,10 @@
+package ch07.p2Polymorphism.Exam;
+
+public class SnowTireEx {
+    public static void main(String[] args) {
+        SnowTire snowTire = new SnowTire();
+
+        Tire tire = new Tire();
+
+    }
+}
