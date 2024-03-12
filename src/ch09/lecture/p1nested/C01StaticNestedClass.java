19e17c74220c5c586394806fe62054d91bd3215d
sehyeon <gs1884@naver.com>
2024-03-12 16:27:43 +0900

    Static Nested Class

---
+++ src/ch09/lecture/p1nested/C01StaticNestedClass.java
@@ -0,0 +1,19 @@
+package ch09.lecture.p1nested;
+
+public class C01StaticNestedClass {
+    public static void main(String[] args) {
+        C01MyClass.C01NestedClass obj1= new C01MyClass.C01NestedClass();
+
+    }
+}
+
+
+class C01MyClass{
+    //정적(static) 중첩 클래스
+    static class C01NestedClass{
+    }
+
+    void method1() {
+        C01NestedClass obj1 = new C01NestedClass();
+    }
+}
