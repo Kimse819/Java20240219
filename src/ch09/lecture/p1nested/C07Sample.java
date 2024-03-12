62ef667dcf2a649db1947b71eef54c9bcfbef48f
sehyeon <gs1884@naver.com>
2024-03-12 16:30:33 +0900

    익명 클래스 만드는 연습

---
+++ src/ch09/lecture/p1nested/C07Sample.java
@@ -0,0 +1,47 @@
+package ch09.lecture.p1nested;
+
+public class C07Sample {
+    public static void main(String[] args) {
+        // 가위 바위 보 게임 익명 클래스 객체 생성
+        Game rpsGame = new Game() {
+            @Override
+            public void play() {
+                String[] options = {"가위", "바위", "보"};
+                int randomIndex = (int) (Math.random() * options.length);
+                System.out.println("가위 바위 보 중 하나: " + options[randomIndex]);
+            }
+        };
+
+        // 가위 바위 보 게임 실행
+        rpsGame.play();
+
+        // 로또 게임 익명 클래스 객체 생성
+        Game lottoGame = new Game() {
+            @Override
+            public void play() {
+                int[] lottoNumbers = new int[6];
+                boolean[] isPicked = new boolean[46]; // 1부터 45까지의 숫자가 나왔는지 체크
+
+                System.out.print("1 ~ 45 사이의 숫자 출력: ");
+                for (int i = 0; i < 6; i++) {
+                    int number;
+                    do {
+                        number = (int) (Math.random() * 45) + 1;
+                    } while (isPicked[number]); // 이미 뽑은 숫자라면 다시 뽑기
+                    lottoNumbers[i] = number;
+                    isPicked[number] = true;
+                    System.out.print(number + " ");
+                }
+                System.out.println();
+            }
+        };
+
+        // 로또 게임 실행
+        lottoGame.play();
+    }
+
+    interface Game {
+        void play();
+    }
+}
+
