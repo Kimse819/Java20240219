8dd545f9ea5d58a8babdb0b066c286af6150e420
sehyeon <gs1884@naver.com>
2024-03-13 17:17:45 +0900

    익명 클래스 만드는 연습

--- src/ch09/lecture/p1nested/p2anonymous/C07Sample.java
+++ src/ch09/lecture/p1nested/p2anonymous/C07Sample.java
@@ -1,4 +1,4 @@
-package ch09.lecture.p1nested;
+package ch09.lecture.p1nested.p2anonymous;
 
 public class C07Sample {
     public static void main(String[] args) {
@@ -8,7 +8,7 @@ public class C07Sample {
             public void play() {
                 String[] options = {"가위", "바위", "보"};
                 int randomIndex = (int) (Math.random() * options.length);
-                System.out.println("가위 바위 보 중 하나: " + options[randomIndex]);
+                System.out.println("가위 바위 보: " + options[randomIndex]);
             }
         };
 
@@ -22,7 +22,7 @@ public class C07Sample {
                 int[] lottoNumbers = new int[6];
                 boolean[] isPicked = new boolean[46]; // 1부터 45까지의 숫자가 나왔는지 체크
 
-                System.out.print("1 ~ 45 사이의 숫자 출력: ");
+                System.out.print("1 ~ 45 사이의 숫자 출력: "); //출력되는 숫자는 중복이 없어야 함
                 for (int i = 0; i < 6; i++) {
                     int number;
                     do {
