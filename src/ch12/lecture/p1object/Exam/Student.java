ee6ab9fb8b4edc6c16c40a4084e055997c1d742a
sehyeon <gs1884@naver.com>
2024-03-14 17:25:29 +0900

    예제

---
+++ src/ch12/lecture/p1object/Exam/Student.java
@@ -0,0 +1,30 @@
+package ch12.lecture.p1object.Exam;
+
+public class Student {
+    private int no;
+    private String name;
+
+    public Student(int no, String name){
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() { return no; }
+    public String getName() { return name; }
+
+//    @Override
+    public int hashcode() {
+        int hashcode = no + name.hashCode();
+        return hashcode;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof Student target) {
+            if (no == target.getNo() && name.equals(target.getName())){
+                return true;
+            }
+        }
+        return false;
+    }
+}
