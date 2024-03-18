89bebaa423f55ac7deebb901b43173aabaa8515e
sehyeon <gs1884@naver.com>
2024-03-18 16:30:28 +0900

    generic method

---
+++ src/ch13/lecture/p1generic/C09GenericMethod.java
@@ -0,0 +1,12 @@
+package ch13.lecture.p1generic;
+
+public class C09GenericMethod {
+    public static void main(String[] args) {
+        C07Box<String> box = new C07Box<>();
+
+    }
+    //제네릭 메소드
+    public static <T> void method(T param){
+
+    }
+}
