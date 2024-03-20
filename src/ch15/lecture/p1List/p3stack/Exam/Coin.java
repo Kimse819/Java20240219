f63fad9e889a5dbd209b6c365dc5f8a8acb56e4e
sehyeon <gs1884@naver.com>
2024-03-20 17:16:05 +0900

    stack

---
+++ src/ch15/lecture/p1List/p3stack/Exam/Coin.java
@@ -0,0 +1,13 @@
+package ch15.lecture.p1List.p3stack.Exam;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
